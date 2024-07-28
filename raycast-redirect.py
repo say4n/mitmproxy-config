b423098fab2162949e4c11b8d8f578090c99092b
Sayan Goswami <goswami.sayan47@gmail.com>
2024-07-28 18:33:37 +0100

    adds raycast-redirect.py

---
+++ raycast-redirect.py
@@ -0,0 +1,11 @@
+"""Redirect HTTP requests to another server."""
+
+from mitmproxy import http
+
+
+def request(flow: http.HTTPFlow) -> None:
+    # pretty_host takes the "Host" header of the request into account,
+    # which is useful in transparent mode where we usually only have the IP
+    # otherwise.
+    if flow.request.pretty_host == "backend.raycast.com":
+        flow.request.host = "fake-raycast-backend-ten.vercel.app"
